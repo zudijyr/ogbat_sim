27bdfbe22e5bd3a7671134e7099514cc20810dff
zydijyr <charliepye@gmail.com>
2017-10-08 13:05:11 -0700

    updates hp after attack

--- ogbat_sim/ogbat_sim.py
+++ ogbat_sim/ogbat_sim.py
@@ -9,6 +9,14 @@ blue_damage = 0
 red_damage = 0
 win = GraphWin('Draw units', 700, 500)
 message = Text(Point(win.getWidth()/2, 30), 'Ogre Battle Fight Sim')
+hp1 = Text(Point(600,390), '0')
+hp2 = Text(Point(650,370), '0')
+hp3 = Text(Point(590,470), '0')
+hp4 = Text(Point(640,440), '0')
+hp5 = Text(Point(200,220), '0')
+hp6 = Text(Point(250,200), '0')
+hp7 = Text(Point(210,170), '0')
+hp8 = Text(Point(260,160), '0')
 
 def does_it_hit(attacker, defender):
     #TODO actual to-hit rules
@@ -75,6 +83,16 @@ def attack(attacker, defender):
             defender.hp = 0
             defender.is_alive = False
 
+def set_hp_text(unit1,unit2):
+    hp1.setText(unit1.characters[0].hp)
+    hp2.setText(unit1.characters[1].hp)
+    hp3.setText(unit1.characters[2].hp)
+    hp4.setText(unit1.characters[3].hp)
+    hp5.setText(unit2.characters[0].hp)
+    hp6.setText(unit2.characters[1].hp)
+    hp7.setText(unit2.characters[2].hp)
+    hp8.setText(unit2.characters[3].hp)
+
 def combat_round(all_chars,unit1,unit2):
     for x in all_chars:
         if x.is_alive == False or x.num_attacks_remaining == 0:
@@ -86,13 +104,7 @@ def combat_round(all_chars,unit1,unit2):
             else:
                 target = choose_target(x,unit1)
             attack(x, target)
-
-    #for x in unit1.characters:
-    #    print(x.name)
-    #    print(x.hp)
-    #for x in unit2.characters:
-    #    print(x.name)
-    #    print(x.hp)
+            set_hp_text(unit1,unit2)
 
 def battle():
     char1_1 = Fighter("fighter 1_1","blue")
@@ -105,6 +117,7 @@ def battle():
     char2_4 = Amazon("amazon 2_4","red")
     unit1 = Unit("blue",[char1_1,char1_2,char1_3,char1_4])
     unit2 = Unit("red",[char2_1,char2_2,char2_3,char2_4])
+    set_hp_text(unit1,unit2)
     all_chars = unit1.characters + unit2.characters
     all_chars.sort(key=lambda x: x.agility, reverse=True)
     #TODO include some luck into sorting
@@ -131,11 +144,11 @@ def draw_stuff():
     message.setSize(20)
     message.draw(win)
 
-    image1 = Image(Point(600,400),'blue_fighter.gif')
+    image1 = Image(Point(600,370),'blue_fighter.gif')
     image1.draw(win)
-    image2 = Image(Point(650,380),'blue_fighter.gif')
+    image2 = Image(Point(650,350),'blue_fighter.gif')
     image2.draw(win)
-    image3 = Image(Point(600,440),'blue_amazon.gif')
+    image3 = Image(Point(590,450),'blue_amazon.gif')
     image3.draw(win)
     image4 = Image(Point(640,420),'blue_amazon.gif')
     image4.draw(win)
@@ -149,7 +162,33 @@ def draw_stuff():
     image8 = Image(Point(260,140),'red_amazon.gif')
     image8.draw(win)
 
-    message.setText('Click anywhere to begin') # change text message
+    hp1.setTextColor('red')
+    hp1.setSize(15)
+    hp1.draw(win)
+    hp2.setTextColor('red')
+    hp2.setSize(15)
+    hp2.draw(win)
+    hp3.setTextColor('red')
+    hp3.setSize(15)
+    hp3.draw(win)
+    hp4.setTextColor('red')
+    hp4.setSize(15)
+    hp4.draw(win)
+
+    hp5.setTextColor('red')
+    hp5.setSize(15)
+    hp5.draw(win)
+    hp6.setTextColor('red')
+    hp6.setSize(15)
+    hp6.draw(win)
+    hp7.setTextColor('red')
+    hp7.setSize(15)
+    hp7.draw(win)
+    hp8.setTextColor('red')
+    hp8.setSize(15)
+    hp8.draw(win)
+
+    message.setText('Click anywhere to begin')
     win.getMouse()
 
 def main(argv):
