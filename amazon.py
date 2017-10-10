0ff3b0c341b0b85f0886898d0f0accb868171ac4
zydijyr <charliepye@gmail.com>
2017-10-09 20:23:26 -0700

    actual stats

--- amazon.py
+++ amazon.py
@@ -1,10 +1,10 @@
 from character import Character
 
 class Amazon(Character):
-    hp = 20
-    strength = 8
-    agility = 12
-    luck = 10
-    intelligence = 7
+    hp = 83
+    strength = 42
+    agility = 50
+    luck = 48
+    intelligence = 52
     num_attacks = 2
     attack_type = 'physical'
--- fighter.py
+++ fighter.py
@@ -1,10 +1,10 @@
 from character import Character
 
 class Fighter(Character):
-    hp = 10
-    strength = 12
-    agility = 8
-    luck = 10
-    intelligence = 5
+    hp = 77
+    strength = 45
+    agility = 47
+    luck = 45
+    intelligence = 40
     num_attacks = 2
     attack_type = 'physical'
